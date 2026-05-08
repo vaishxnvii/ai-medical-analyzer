65b5cc01ac911f197f5b242b2b595301edcc7776
vaishxnvii <vaishnavibhardwaj004@gmail.com>
2026-05-08 21:24:01 +0530

    Add files via upload

---
+++ BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package clinical_ai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
